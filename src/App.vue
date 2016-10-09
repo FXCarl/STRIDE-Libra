0a8a32aaca87a5f852e6fc9b96c0ca1880ec40fb
FXCarl <fxcarlcn@gmail.com>
2016-10-09 15:47:11 +0800

    vue with webpack

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template  src="./assets/html/main.html"></template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue From FXCarl'
+    }
+  }
+}
+</script>
+
+<style src="./assets/css/style_index.css"></style>
