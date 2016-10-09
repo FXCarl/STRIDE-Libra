9512ce201dba680c87edc87bece9b88b994754fd
FXCarl <fxcarlcn@gmail.com>
2016-10-10 00:32:17 +0800

    work with vue router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+Vue.use(VueRouter)
+Vue.use(Vuex)
+
 import App from './App.vue'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+const routes = [
+  { path: '/intro', component: App }
+]
+
+// keep it simple for now.
+const router = new VueRouter({
+  routes
 })
+
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app = new Vue({
+  router
+}).$mount('#app')
+
+// Now the app has started!
+router.push('/intro')
