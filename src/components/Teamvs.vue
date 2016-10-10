4c5e134d427460dcc27999a020c5862679645861
Shiyuu <297116296@qq.com>
2016-10-11 00:12:40 +0800

    add member list

--- src/components/Teamvs.vue
+++ src/components/Teamvs.vue
@@ -4,6 +4,79 @@
 
         <div class="page_bg hd"></div>
 
+        <div class="members_box ui">
+            <div class="members_box_under ui"></div>
+            <div class="focus_order_p"></div>
+            <div class="members_box_main ui">
+                <div class="members_list">
+                    <div class="members_list_e" id="mbr_list_1">
+                        <div class="ui" id="mbr_char_color_1"></div>
+                        <div class="ui" id="mbr_char_white_1"></div>
+                        <div class="ui" id="mbr_char_avt_1"></div>
+                        <div class="ui" id="mbr_char_nm_1">
+                            <span id="mbr_char_nm_cnt_1">角元明日香</span>
+                        </div>
+                        <div class="ui" id="mbr_char_p_1">
+                            <span id="mbr_char_p_cnt_1">2</span>
+                        </div>
+                    </div>
+                    <div class="members_list_e" id="mbr_list_2">
+                        <div class="ui" id="mbr_char_color_2"></div>
+                        <div class="ui" id="mbr_char_white_2"></div>
+                        <div class="ui" id="mbr_char_avt_2"></div>
+                        <div class="ui" id="mbr_char_nm_2">
+                            <span id="mbr_char_nm_cnt_2">大岛美咲</span>
+                        </div>
+                        <div class="ui" id="mbr_char_p_2">
+                            <span id="mbr_char_p_cnt_2"> </span>
+                        </div>
+                    </div>
+                    <div class="members_list_e" id="mbr_list_3">
+                        <div class="ui" id="mbr_char_color_3"></div>
+                        <div class="ui" id="mbr_char_white_3"></div>
+                        <div class="ui" id="mbr_char_avt_3"></div>
+                        <div class="ui" id="mbr_char_nm_3">
+                            <span id="mbr_char_nm_cnt_3">大森日雅</span>
+                        </div>
+                        <div class="ui" id="mbr_char_p_3">
+                            <span id="mbr_char_p_cnt_3"> </span>
+                        </div>
+                    </div>
+                    <div class="members_list_e" id="mbr_list_4">
+                        <div class="ui" id="mbr_char_color_4"></div>
+                        <div class="ui" id="mbr_char_white_4"></div>
+                        <div class="ui" id="mbr_char_avt_4"></div>
+                        <div class="ui" id="mbr_char_nm_4">
+                            <span id="mbr_char_nm_cnt_4">大岛美咲</span>
+                        </div>
+                        <div class="ui" id="mbr_char_p_4">
+                            <span id="mbr_char_p_cnt_4"> </span>
+                        </div>
+                    </div>
+                    <div class="members_list_e" id="mbr_list_5">
+                        <div class="ui" id="mbr_char_color_5"></div>
+                        <div class="ui" id="mbr_char_white_5"></div>
+                        <div class="ui" id="mbr_char_avt_5"></div>
+                        <div class="ui" id="mbr_char_nm_5">
+                            <span id="mbr_char_nm_cnt_5">大岛美咲</span>
+                        </div>
+                        <div class="ui" id="mbr_char_p_5">
+                            <span id="mbr_char_p_cnt_5">1</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="members_box">
+                    <div class="members_box_status_bar"></div>
+                    <div class="members_box_status_1"></div>
+                    <div class="members_box_status_2"></div>
+                    <div class="members_box_status_3"></div>
+                    <div class="members_box_status_4"></div>
+                    <div class="members_box_status_5"></div>
+                    <div class="members_box_status_6"></div>
+                </div>
+            </div>
+        </div>
+
         <div class="team_vs_order_bar ui">
             <div class="team_vs_order_bar_under ui"></div>
             <div class="team_vs_order_bar_position" id="odr_bar_p1">
@@ -32,6 +105,8 @@
                 <div class="order_bar_lock_icon" id="odr_bar_lck_5"></div>
             </div>
         </div>
+
+        
 
     </div>
 
