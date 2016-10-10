adf2e6f84e6354d79d37aabcf4798532a3396a07
chilzyj <zyjchil@163.com>
2016-10-10 20:07:00 +0800

    Add Qrcode

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,10 @@
 import App from '../components/App.vue'
+import Qrpage from '../components/Qrpage.vue'
 import Teamvs from '../components/Teamvs.vue'
 
 // route map
 export default [
   { path: '', component: App },
-  { path: '/team', component: Teamvs}
+  { path: '/team', component: Teamvs},
+  { path: '/qrcode', component: Qrpage}
 ]
