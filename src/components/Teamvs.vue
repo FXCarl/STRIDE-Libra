d0f9e938471fd333d9bad67388b077b3d7fde7cf
Shiyuu <297116296@qq.com>
2016-10-10 20:20:10 +0800

    Add teamvs.vue

--- src/components/Teamvs.vue
+++ src/components/Teamvs.vue
@@ -2,18 +2,35 @@
 
     <div class="wrapper">
 
-        <span>This is VS page.</span>
+        <div class="page_bg hd"></div>
 
-        <div class="team_vs_order_bar">
-            <div class="team_vs_order_bar_under"></div>
+        <div class="team_vs_order_bar ui">
+            <div class="team_vs_order_bar_under ui"></div>
             <div class="team_vs_order_bar_position" id="odr_bar_p1">
                 <div class="order_bar_content" id="odr_bar_cnt_1"></div>
+                <div class="order_bar_content order_bar_selected" id="odr_bar_slc_1"></div>
                 <div class="order_bar_lock_icon" id="odr_bar_lck_1"></div>
             </div>
-            <div class="team_vs_order_bar_position odr_p2"></div>
-            <div class="team_vs_order_bar_position odr_p3"></div>
-            <div class="team_vs_order_bar_position odr_p4"></div>
-            <div class="team_vs_order_bar_position odr_p5"></div>
+            <div class="team_vs_order_bar_position" id="odr_bar_p2">
+                <div class="order_bar_content" id="odr_bar_cnt_2"></div>
+                <div class="order_bar_content order_bar_selected" id="odr_bar_slc_2"></div>
+                <div class="order_bar_lock_icon" id="odr_bar_lck_2"></div>
+            </div>
+            <div class="team_vs_order_bar_position" id="odr_bar_p3">
+                <div class="order_bar_content" id="odr_bar_cnt_3"></div>
+                <div class="order_bar_content order_bar_selected" id="odr_bar_slc_3"></div>
+                <div class="order_bar_lock_icon" id="odr_bar_lck_3"></div>
+            </div>
+            <div class="team_vs_order_bar_position" id="odr_bar_p4">
+                <div class="order_bar_content" id="odr_bar_cnt_4"></div>
+                <div class="order_bar_content order_bar_selected" id="odr_bar_slc_4"></div>
+                <div class="order_bar_lock_icon" id="odr_bar_lck_4"></div>
+            </div>
+            <div class="team_vs_order_bar_position" id="odr_bar_p5">
+                <div class="order_bar_content" id="odr_bar_cnt_5"></div>
+                <div class="order_bar_content order_bar_selected" id="odr_bar_slc_5"></div>
+                <div class="order_bar_lock_icon" id="odr_bar_lck_5"></div>
+            </div>
         </div>
 
     </div>
