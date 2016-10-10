249740e6230124b32e20783f30c5fa02daf19fe5
chilzyj <zyjchil@163.com>
2016-10-10 19:58:59 +0800

    Add QRCode

---
+++ src/components/Qrpage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div id = 'hint'>This is a QRCode Page!</div>
+        <div id = 'qrcode'></div>
+        <button v-on:click = 'create'>QRCode</button>
+    </div>
+</template>
+<script>
+    import QRCode from 'davidshimjs-qrcodejs'
+    export default{
+        methods:{
+            create:function(){
+                new QRCode(document.getElementById("qrcode"), "http://www.baidu.com");
+            }
+        }
+    }
+</script>
+<style>
+    .hint{background-color:#ffffff;height:40px}
+</style>
