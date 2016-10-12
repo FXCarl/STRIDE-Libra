77ecbd1b7f36e9ffa45689aea6d4d325f33ed572
Shiyuu <297116296@qq.com>
2016-10-12 22:45:02 +0800

    add list
    main.html "#btn_3" router-link to "/list"
    List.vue ".list_info_button_detail" link to "/detail"
    List.vue ".list_info_button_change" link to "/garage"

--- src/components/List.vue
+++ src/components/List.vue
@@ -6,10 +6,47 @@
 
         <div class="list_team ui">
 
-            <div class="list_team_box"></div>
-            <div class="list_team_box"></div>
-            <div class="list_team_box"></div>
-            
+            <div class="list_team_box_01 ui"></div>
+            <div class="list_team_box_02 ui"></div>
+            <div class="list_team_box_03 ui"></div>
+            <div class="list_team_box_04 ui"></div>
+            <div class="list_team_box_05 ui"></div>
+            <div class="list_team_box_06 ui"></div>
+            <div class="list_team_box_07 ui"></div>
+            <div class="list_team_box_08 ui"></div>
+            <div class="list_team_box_09 ui"></div>
+            <div class="list_team_box_10 ui"></div>
+
+        </div>
+
+        <div class="list_avatar ui"></div>
+
+        <div class="list_info_name ui">
+            <span>Name</span>
+        </div>
+
+        <div class="list_info_status ui">
+            <div>Long</div>
+            <div>Long</div>
+            <div>Long</div>
+            <div>Long</div>
+            <div>Long</div>
+            <div>Long</div>
+        </div>
+
+        <div class="list_info_skill ui">
+            <div>Skill</div>
+            <div>Skill</div>
+        </div>
+
+        <div class="list_info_buff ui">
+            <div>Buff:71%</div>
+            <div>Life:82%</div>
+        </div>
+
+        <div class="list_info_buttons ui">
+            <router-link to="/detail"><div class="list_info_button_detail">Detail</div></router-link>
+            <router-link to="/garage"><div class="list_info_button_change">Change</div></router-link>
         </div>
 
     </div>
